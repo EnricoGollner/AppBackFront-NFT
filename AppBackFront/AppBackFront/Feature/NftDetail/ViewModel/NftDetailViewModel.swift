f2a9ed9b43cc1e112820042dddf72a9eb795f109
EnricoGollner <enricogollner.tech@gmail.com>
2023-05-11 22:34:24 -0300

    Criando tela de detalhes

---
+++ AppBackFront/AppBackFront/Feature/NftDetail/ViewModel/NftDetailViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  NftDetailViewModel.swift
+//  AppBackFront
+//
+//  Created by Enrico Sousa Gollner on 11/05/23.
+//
+
+import UIKit
+
+class NftDetailViewModel {
+    let nft: Nft
+    
+    init(nft: Nft) {
+        self.nft = nft
+    }
+}
